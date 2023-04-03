a3e1b734c2ce1ef9f78ebe7ff5791c48f1ba3e2e
Gordon Clark <gordon.clark@botpress.com>
2023-04-03 09:52:06 -0400

    Create callSpoonacularAPI.js

---
+++ callSpoonacularAPI.js
@@ -0,0 +1,21 @@
+var options = {
+  method: 'GET',
+  url: 'https://api.spoonacular.com/recipes/complexSearch',
+  params: {
+    query: workflow.baseQuery,
+    diet: encodeURIComponent(workflow.dietaryRestrictions),
+    apiKey: env.apiKey,
+    number: '3',
+    ignorePantry: 'false',
+    sort: 'popularity',
+    sortDirection: 'asc',
+    addRecipeInformation: 'true',
+    addRecipeNutrition: 'false'
+  }
+}
+const response = await axios.request(options)
+if (response.status == 200) {
+  workflow.recipes = response.data.results
+} else {
+  console.log(`API call failed with status code ${response.status}`)
+}
