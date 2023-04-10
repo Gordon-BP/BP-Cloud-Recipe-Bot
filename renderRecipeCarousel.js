589153d3c789b76326319d3adae0a52f21932350
Gordon Clark <gordon.clark@botpress.com>
2023-04-10 05:48:04 -0400

    Update renderRecipeCarousel.js
    Removed Promise.all, there's no need for this to be async

--- renderRecipeCarousel.js
+++ renderRecipeCarousel.js
@@ -1,44 +1,42 @@
 workflow.recipeInfo = []
-const myCards = await Promise.all(
-  workflow.recipes.map(async (recipe) => {
-    workflow.recipeInfo.push({
-      title: recipe.title,
-      vegetarian: recipe.vegetarian,
-      vegan: recipe.vegan,
-      glutenFree: recipe.glutenFree,
-      dairyFree: recipe.dairyFree,
-      veryHealthy: recipe.veryHealthy,
-      cheap: recipe.cheap,
-      veryPopular: recipe.veryPopular,
-      readyInMinutes: recipe.readyInMinutes,
-      servings: recipe.servings,
-      summary: recipe.summary
-    })
-    // create the card object
-    return {
-      type: 'card',
-      title: {
-        dynamicValue: `${recipe.title}`,
-        valueType: 'dynamic'
-      },
-      subtitle: {
-        dynamicValue: '',
-        valueType: 'dynamic'
-      },
-      imageUrl: {
-        dynamicValue: `${recipe.image}`,
-        valueType: 'dynamic'
-      },
-      actions: [
-        {
-          action: 'url',
-          label: 'View Recipe',
-          value: `${recipe.sourceUrl}`
-        }
-      ]
-    }
+const myCards = workflow.recipes.map((recipe) => {
+  workflow.recipeInfo.push({
+    title: recipe.title,
+    vegetarian: recipe.vegetarian,
+    vegan: recipe.vegan,
+    glutenFree: recipe.glutenFree,
+    dairyFree: recipe.dairyFree,
+    veryHealthy: recipe.veryHealthy,
+    cheap: recipe.cheap,
+    veryPopular: recipe.veryPopular,
+    readyInMinutes: recipe.readyInMinutes,
+    servings: recipe.servings,
+    summary: recipe.summary
   })
-)
+  // create the card object
+  return {
+    type: 'card',
+    title: {
+      dynamicValue: `${recipe.title}`,
+      valueType: 'dynamic'
+    },
+    subtitle: {
+      dynamicValue: '',
+      valueType: 'dynamic'
+    },
+    imageUrl: {
+      dynamicValue: `${recipe.image}`,
+      valueType: 'dynamic'
+    },
+    actions: [
+      {
+        action: 'url',
+        label: 'View Recipe',
+        value: `${recipe.sourceUrl}`
+      }
+    ]
+  }
+})
 workflow.cards = []
 for (var card of myCards) {
   workflow.cards.push({
